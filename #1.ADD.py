cd5cf9c297fa11a41605d662615613dd3d32ffcc
earnest-gakure <105698709+earnest-gakure@users.noreply.github.com>
2022-05-18 22:47:09 -0700

    Create #1.ADD.py

---
+++ #1.ADD.py
@@ -0,0 +1,18 @@
+#1.ADD
+#2.SUBTRACT
+#3.MULTIPLY
+#4.DIVIDE
+print("select an operation system:")
+print("1.ADD")
+print("2.SUBTRACT")
+print("3.MULTIPLY")
+print("4.DIVIDE")
+
+operation=input()
+if operation =1
+#code for ADD
+elif operation =2
+#code for SUBTRACT
+elif operation
+
+)
